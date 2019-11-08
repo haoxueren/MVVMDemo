bd178b37b432fbe130d39acecac92242d8d92868
haoxueren <haoxueren@foxmail.com>
2019-11-09 02:38:24 +0800

    添加了两个测试方法(双向绑定)

--- app/src/main/java/com/haoxueren/mvvmdemo/MainActivity.java
+++ app/src/main/java/com/haoxueren/mvvmdemo/MainActivity.java
@@ -13,14 +13,29 @@ import com.haoxueren.mvvmdemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
 
+    private Person person;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = DataBindingUtil
                 .setContentView(this, R.layout.activity_main);
-        Person person = new Person();
+        person = new Person();
         person.name.set("haoxueren");
         binding.setPerson(person);
     }
 
+    /**
+     * 更新对象的值，请注意观察控件内容的变化
+     */
+    public void updatePerson(View view) {
+        person.name.set("大智先生");
+    }
+
+    /**
+     * 控件内容变化后，Toast对象的值
+     */
+    public void queryPerson(View view) {
+        Toast.makeText(this, person.name.get(), Toast.LENGTH_SHORT).show();
+    }
 }
